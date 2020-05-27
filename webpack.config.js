227aa163d919a226dca39aa522edb5aa1fd50c7c
Razielini <razielco@gmail.com>
2020-05-26 21:16:56 -0600

    Almost Finish

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const copyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -28,6 +29,14 @@ module.exports = {
         template: './public/index.html',
         filename: './index.html'
       }
-    )
+    ),
+    new copyWebpackPlugin({
+      patterns: [
+        {
+          from: './src/styles/styles.css',
+          to: ''
+        }
+      ]
+    })
   ]
 }
